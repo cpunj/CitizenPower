9a182ce67ab86e64d2635ddd6122cb6e236c817f
Chandan Punj <punjchandan@gmail.com>
2020-09-04 18:05:52 +1000

    added search bar and icon in search.dart file

--- home/lib/Views/MessageViews/search.dart
+++ home/lib/Views/MessageViews/search.dart
@@ -1,3 +1,4 @@
+import 'package:citizenpower/Layouts/GenericLayouts.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -9,6 +10,40 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen>{
   @override   
   Widget build(BuildContext context){
-    return Container();
+    return Scaffold(
+      appBar:topAppBarLayout("Search"),
+      body:Container(
+        child: Column(children: 
+        [
+          Container(
+            color:Color(0x54FFFFFF),
+            padding:EdgeInsets.symmetric(horizontal:24,vertical:16),
+            child:
+          Row(children: [
+          Expanded(
+            child:TextField(
+              style:TextStyle(
+                color:Colors.white
+              ),
+              decoration: new InputDecoration(
+                hintText:"Search username",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30.0)),
+                              hintStyle: TextStyle(
+                                color: Colors.white54,
+                              ),
+              )
+            ),
+          ), 
+   
+          Image.asset("assets/download.png",
+          height:70,
+          width:70)],
+          ),
+          ),
+        ],) ,
+      )
+      
+    );
   }
 }
