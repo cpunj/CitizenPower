3e91270969aea0a6d786965bf8c08ea721ec2b22
Harry <harrison.fawcett@utas.edu.au>
2020-09-04 20:50:36 +1000

    First help page commit. Existing problems with scrollable list overflows to be fixed.

--- home/lib/views/helpView/helpViewWidgets/faq_list.dart
+++ home/lib/views/helpView/helpViewWidgets/faq_list.dart
@@ -24,7 +24,6 @@ TextStyle responseStyle() {
 
 Card faqListItem(String question, String response){
   // A function which adds an item to the FAQ list. It takes in 2 String values when called.
-
   return Card(
     elevation: 2,
     child: new Container(
@@ -94,52 +93,58 @@ class FAQList extends StatelessWidget {
             height: 10,
           ),
 
-          SingleChildScrollView(
+    // TODO Fix overflow and make this list scrollable
+          Expanded(child:
+            SingleChildScrollView(
             scrollDirection: Axis.vertical,
-            child: Container(
+
               child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    faqListItem("How do I make a post?",
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                children: <Widget>[
+                  faqListItem("How do I make a post?",
                         "Simply click the +1 New Post Button."),
+                  SizedBox(
+                    height: 10,
+                  ),
 
-                    SizedBox(
-                      height: 10,
-                    ),
+                  faqListItem("How do I know which electorate I am in?",
+                      "Please visit the 'Electorate' tab."),
 
-                    faqListItem("How do I know which electorate I am in?",
-                        "Please visit the 'Electorate' tab."),
+                  SizedBox(
+                    height: 10,
+                  ),
 
-                    SizedBox(
-                      height: 10,
-                    ),
+                  faqListItem("What happens if I forget my password?",
+                      "You can access the forgot password feature of the application from the login screen. "
+                          "If you are logged in, you can 'logout' through the settings menu, "
+                          "from there you can access the 'forgot password feature."),
 
-                    faqListItem("How do I know which electorate I am in?",
-                        "Please visit the 'Electorate' tab."),
+                  SizedBox(
+                    height: 10,
+                  ),
 
-                    SizedBox(
-                      height: 10,
-                    ),
+                  faqListItem("What if someone is saying something hateful on the app?",
+                      "You can report behaviour which you believe is not in accordance with our "
+                          "community guidelines. A moderator will review your report and remove the content "
+                          "if it contravenes the guidelines."),
 
-                    faqListItem("How do I know which electorate I am in?",
-                        "Please visit the 'Electorate' tab."),
+                  SizedBox(
+                    height: 10,
+                  ),
 
-                    SizedBox(
-                      height: 10,
-                    ),
+                  faqListItem("Can I trust the information on the app?",
+                      "Post which have a tick next to them are verified - which means they have been "
+                          "created by the Citizen Tasmania team. We always advise conducting your own "
+                          "research on a topic, but you can rest assured the claims made in those posts"
+                          "are well researched."),
 
-                    faqListItem("How do I know which electorate I am in?",
-                        "Please visit the 'Electorate' tab."),
-
-                    SizedBox(
-                      height: 10,
-                    ),
-
-                    faqListItem("How do I know which electorate I am in?",
-                        "Please visit the 'Electorate' tab."),
-
-                  ],
-                ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
             ),
           ),
         ],
