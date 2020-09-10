eb5f2c37529a08518a4942997ceb1de59402dc06
jackl <jack.lennard.00@gmail.com>
2020-09-11 01:01:09 +1000

    Commented out post list, need to use stream builder, will research tomorrow

--- home/lib/views/profileViews/my_profile.dart
+++ home/lib/views/profileViews/my_profile.dart
@@ -134,15 +134,16 @@ class _MyProfilePageState extends State<MyProfilePage> {
                     height: 10,
                     color: Colors.black,
                   ),
-                  ListView.builder(
-                      shrinkWrap: true,
-                      itemCount:
-                          profileController.postListSnapshot.documents.length,
-                      itemBuilder: (context, index) {
-                        print(profileController.postListSnapshot);
-                        return Image.network(profileController
-                            .postListSnapshot.documents[index].data["picLink"]);
-                      })
+//Need to use a stream builder it would seem
+//                  ListView.builder(
+//                      shrinkWrap: true,
+//                      itemCount:
+//                          profileController.postListSnapshot.documents.length,
+//                      itemBuilder: (context, index) {
+//                        print(profileController.postListSnapshot);
+//                        return Image.network(profileController
+//                            .postListSnapshot.documents[index].data["picLink"]);
+//                      })
                 ]),
               ),
             ),
