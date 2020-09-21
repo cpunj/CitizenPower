dc1c6526896c55557ad90b1fd4129179e4dda725
jackl <jack.lennard.00@gmail.com>
2020-09-21 14:10:49 +1000

    Fixed merge errors

--- home/lib/login_page.dart
+++ home/lib/login_page.dart
@@ -42,145 +42,142 @@ class LoginPageState extends State<LoginPage> {
       ),
       //Q: Any particular reason for using a stack rather than a column here? -Jack
       body: SafeArea(
-        child: new Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            new Form(
-                key: _formKey,
-                child: new Theme(
-                  data: new ThemeData(
-                    brightness: Brightness.light,
-                  ),
-                  child: new Container(
-                    padding: const EdgeInsets.only(top: 25),
-                    height: 160,
-                    width: 100,
-                    child: new Column(children: <Widget>[
-                      new TextFormField(
+        child: new Stack(fit: StackFit.expand, children: <Widget>[
+          new Form(
+            key: _formKey,
+            child: new Theme(
+              data: new ThemeData(
+                brightness: Brightness.light,
+              ),
+              child: new Container(
+                padding: const EdgeInsets.only(top: 25),
+                height: 160,
+                width: 100,
+                child: new Column(
+                  children: <Widget>[
+                    new TextFormField(
+                        decoration: new InputDecoration(
+                            contentPadding:
+                                EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 20.0),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40.0)),
+                            hintStyle: TextStyle(color: Colors.white),
+                            errorStyle: errorTextStyle1(),
+                            labelText: "Email",
+                            hintText: "Enter Email address"),
+                        controller: emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (val) {
+                          if (val.isEmpty ||
+                              !val.contains('@') ||
+                              !val.contains('.') ||
+                              val.length < 8) {
+                            return "Please enter a valid email";
+                          }
+                          return null;
+                        }),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        new TextFormField(
                           decoration: new InputDecoration(
-                              contentPadding:
-                                  EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 20.0),
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(40.0)),
-                              hintStyle: TextStyle(color: Colors.white),
-                              errorStyle: errorTextStyle1(),
-                              labelText: "Email",
-                              hintText: "Enter Email address"),
-                          controller: emailController,
-                          keyboardType: TextInputType.emailAddress,
-                          validator: (val) {
-                            if (val.isEmpty ||
-                                !val.contains('@') ||
-                                !val.contains('.') ||
-                                val.length < 8) {
-                              return "Please enter a valid email";
-                            }
-                            return null;
-                          }),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: <Widget>[
-                          new TextFormField(
-                            decoration: new InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(30.0)),
-                              hintStyle: TextStyle(
-                                color: Colors.white,
-                              ),
-                              errorStyle: TextStyle(
-                                color: Colors.redAccent,
-                                fontSize: 15,
-                              ),
-                              labelText: "Password",
-                              hintText: "Insert password",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30.0)),
+                            hintStyle: TextStyle(
+                              color: Colors.white,
                             ),
-                            keyboardType: TextInputType.text,
-                            controller: passController,
-                            validator: (val) =>
-                                val.length < 6 ? 'Invalid Password' : null,
-                            obscureText: true,
+                            errorStyle: TextStyle(
+                              color: Colors.redAccent,
+                              fontSize: 15,
+                            ),
+                            labelText: "Password",
+                            hintText: "Insert password",
                           ),
-                          SizedBox(
-                            height: 15.0,
+                          keyboardType: TextInputType.text,
+                          controller: passController,
+                          validator: (val) =>
+                              val.length < 6 ? 'Invalid Password' : null,
+                          obscureText: true,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        ForgotPasswordScreen()));
+                          },
+                          child: Text(
+                            "Forgot Password?",
+                            style: TextStyle(color: Colors.red),
                           ),
-                          InkWell(
-                            onTap: () {
+                        ),
+                      ],
+                    ),
+                    new Padding(
+                      padding: const EdgeInsets.only(top: 80),
+                    ),
+                    Center(
+                      child: Material(
+                        elevation: 5.0,
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: darkGold,
+                        child: new MaterialButton(
+                            textColor: Colors.white,
+                            child: new Text("LOGIN"),
+                            onPressed: () {
+                              signIn();
+                            }),
+                      ),
+                    ),
+                    new Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                    ),
+                    Center(
+                      child: Material(
+                        elevation: 5.0,
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: darkGold,
+                        child: new MaterialButton(
+                            textColor: Colors.white,
+                            minWidth: 150,
+                            materialTapTargetSize: MaterialTapTargetSize.padded,
+                            child: new Text("REGISTER"),
+                            onPressed: () {
+                              if (_formKey.currentState.validate()) {
+                                // If the form is valid, display a Snackbar.
+                                Scaffold.of(context).showSnackBar(
+                                    SnackBar(content: Text('Processing Data')));
+                              }
                               Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (BuildContext context) =>
-                                          ForgotPasswordScreen()));
-                            },
-                            child: Text(
-                              "Forgot Password?",
-                              style: TextStyle(color: Colors.red),
-                            ),
-                          ),
-                        ],
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Registration()),
+                              );
+                            }),
                       ),
-                      new Padding(
-                        padding: const EdgeInsets.only(top: 80),
-                      ),
-                      Center(
-                        child: Material(
-                          elevation: 5.0,
-                          borderRadius: BorderRadius.circular(20.0),
-                          color: darkGold,
-                          child: new MaterialButton(
-                              textColor: Colors.white,
-                              child: new Text("LOGIN"),
-                              onPressed: () {
-                                signIn();
-                              }),
-                        ),
-                      ),
-                      new Padding(
-                        padding: const EdgeInsets.only(top: 20.0),
-                      ),
-                      Center(
-                        child: Material(
-                          elevation: 5.0,
-                          borderRadius: BorderRadius.circular(20.0),
-                          color: darkGold,
-                          child: new MaterialButton(
-                              textColor: Colors.white,
-                              minWidth: 150,
-                              materialTapTargetSize:
-                                  MaterialTapTargetSize.padded,
-                              child: new Text("REGISTER"),
-                              onPressed: () {
-                                if (_formKey.currentState.validate()) {
-                                  // If the form is valid, display a Snackbar.
-                                  Scaffold.of(context).showSnackBar(SnackBar(
-                                      content: Text('Processing Data')));
-                                }
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => Registration()),
-                                );
-                              }),
-                        ),
-                      ),
+                    ),
 
-                          //Contains Citizen logo
-                          Center(
-                            child: Container(
-                              height: 200,
-                              padding: EdgeInsets.only(top: 150),
-                              child: new Text("Together We Stand"),
-                            ),
-                          )
-                        ],
+                    //Contains Citizen logo
+                    Center(
+                      child: Container(
+                        height: 200,
+                        padding: EdgeInsets.only(top: 150),
+                        child: new Text("Together We Stand"),
                       ),
-                    ]),
-                  ),
-                ))
-          ],
-        ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
